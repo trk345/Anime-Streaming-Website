d5e2d4bb37fbc6d4d3beb85e2bfddb97fe18ec0f
trk345 <142532975+trk345@users.noreply.github.com>
2024-09-08 12:30:59 +0600

    Delete cse470-Anime-streaming-main directory

--- cse470-Anime-streaming-main/backend/models/genre.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const genreSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true }
-});
-
-const Genre = mongoose.model('Genre', genreSchema);
-
-export default Genre;
--- cse470-Anime-streaming-main/backend/routes/episode.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import * as episodeController from '../controllers/episodes.conroller.js';
-// Routes for episodes
-router.get('/:id', episodeController.getEpisodesByAnimeId);
-router.post('/:id', episodeController.createEpisode);
-
-
-export default router;
